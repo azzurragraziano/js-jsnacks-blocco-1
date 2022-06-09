dafac167118dc2cfe8852d83ded33df6b3fc2761
azzurragraziano <azzurragraziano02@email.com>
2022-06-09 16:56:41 +0200

    impovements

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,6 +1,7 @@
 /* Chiedi un numero di 4 cifre all’utente nel prompt
 e calcola la somma di tutte le cifre che compongono il numero */
 
+// chiedo all'utente un numero di 4 cifre
 let userNumber = prompt('dimmi un numero di 4 cifre')
 let sum = 0;
 
@@ -10,9 +11,10 @@ for (let i = 0; i < userNumber.length; i++) {
     // per ogni numero 
     let singleNumber = parseInt(userNumber[i]);
    
-    // sommo a 0 i numeri dell'utente uno ad uno
+    // sommo a 0 tutte le cifre che compongono il numero
     sum += singleNumber;
 
 }
 
+// stampo in console il risultato 
 console.log(sum);
