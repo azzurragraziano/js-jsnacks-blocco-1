9b3f1ae8d0752f60d1e07f610cbeea9815c9f958
azzurragraziano <azzurragraziano02@email.com>
2022-06-09 16:44:06 +0200

    completed

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,3 +1,25 @@
 /* Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array. */
+
+// creo un array vuoto all'interno del quale andranno i numeri dispari
+const oddNumbers = [];
+
+// faccio ripetere la domanda all'utente per 6 volte (i < 6)
+for (let i = 0; i < 6; i++) {
+
+    // chiedo all'utente un numero
+    userNumber = parseInt( prompt('dimmi un numero') );
+
+    // SE il numero è dispari lo aggiungo all'array vuoto
+    if (userNumber % 2 !== 0) {
+        oddNumbers.push(userNumber)
+    }
+
+    // stampo in console l'array di numeri dispari
+    console.log('oddNumbers', oddNumbers)
+}
+   
+        
+    
+
