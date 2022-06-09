d4a2084e6898b17064d6537369f1096688775892
azzurragraziano <azzurragraziano02@email.com>
2022-06-09 16:54:15 +0200

    completed

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,3 +1,21 @@
 /* Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
 
+let sum = 0;
+
+// faccio ripetere la domanda all'utente per 10 volte (i < 10)
+for (let i = 0; i < 10; i++) {
+
+    // chiedo all'utente un numero
+    userNumber = parseInt( prompt('dimmi un numero') );
+
+    // sommo i numeri dati dall'utente
+    sum += userNumber;
+
+}
+
+// stampo in console il risultato
+console.log(sum)
+
+
+
